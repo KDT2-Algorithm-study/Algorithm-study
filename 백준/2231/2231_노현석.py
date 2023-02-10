93103df00a20f036050d5868b720babef09db2bc
hyundol2 <nhs503601@gmail.com>
2023-02-10 18:29:40 +0900

    Update 2231_노현석.py

--- 백준/2231/2231_노현석.py
+++ 백준/2231/2231_노현석.py
@@ -1,15 +1,17 @@
+# 각 자리수의 합 반환해주는 함수
 def div_add(n):
     num = [int(i) for i in str(n)]
     return sum(num)
 
 n = int(input())
 res = 0
-cnt = 0
 
-for _ in range(n):
-    if n == (cnt + div_add(cnt)):
-        res = cnt
+# 0부터 n까지 순회
+for num in range(1, n+1):
+    if n == (num + div_add(num)): # n이 생성자라면
+
+        # 결과값 변수에 담고 반복문 탈출
+        res = num 
         break
-    cnt += 1
 
 print(res)
