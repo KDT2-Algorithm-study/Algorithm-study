055c6acab7555a0849bf23c4f1890755438e9163
ParkJjHwan22 <bat522@naver.com>
2023-02-12 22:37:31 +0900

    Add 1003_박지환

--- 백준/1003/1003_박지환.py
+++ 백준/1003/1003_박지환.py
@@ -1,13 +1,13 @@
 T = int(input())
 
+def fibonacci(n):
+    zero, one = 0, 1
+    for i in range(n):
+        zero, one = one, zero + one
+    return zero
+
 for _ in range(T):
     n = int(input())
-
-    def fibonacci(n):
-        zero, one = 0, 1
-        for i in range(n):
-            zero, one = one, zero + one
-        return zero
 
     if n == 0:
         print(1, 0)
