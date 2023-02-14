7b004d58896ea4a78bc18a670a08bfd72fb8e906
jeongyun11 <uwn1004@gmail.com>
2023-02-14 16:47:16 +0900

    Add 2609_정윤원_code_2

--- 백준/2609/2609_정윤원.py
+++ 백준/2609/2609_정윤원.py
@@ -1,3 +1,4 @@
+'''
 a, b = map(int, input().split())
 
 if min(a,b) == 1 :
@@ -32,4 +33,28 @@ else :
         if a == b == 1 :
             break
     print(result)
-    
+'''
+
+# 230214 16 : 47 수정 - 'Add 2609_정윤원_code_2'
+
+a, b = map(int, input().split())
+
+if min(a,b) == 1 :
+    print(1)
+    print(max(a,b))
+
+else :
+    i = 2
+    result = 1
+
+    while i <= min(a, b) :
+        if a % i == 0 and b % i == 0 :
+            a = a//i
+            b = b//i
+            result *= i
+        else : 
+            i += 1
+    print(result)
+
+    result *= a*b
+    print(result)
