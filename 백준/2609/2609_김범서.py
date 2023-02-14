f02f3227f405957958f7db3c3e040c34b3a9f2c5
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-02-14 14:01:59 +0900

    Revise 2609_김범서

--- 백준/2609/2609_김범서.py
+++ 백준/2609/2609_김범서.py
@@ -1,1 +1,15 @@
+# 유클리드 호제법
+# 두 수의 최대공약수를 구할 수 있다.
+def get_gcd(n1, n2):
+    while True:
+        if not (n1 % n2):
+            return n2
+        n1, n2 = n2, n1 % n2
 
+number = list(map(int, input().split()))
+large = max(number)
+small = min(number)
+gcd = get_gcd(large, small)
+
+print(gcd)
+print(large * small // gcd)
