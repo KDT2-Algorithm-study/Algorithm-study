b3455e67c4f467e256064c0878074c53c55f586d
hyundol2 <nhs503601@gmail.com>
2023-02-14 23:25:45 +0900

    ADD 2609_노현석

---
+++ 백준/2609/2609_노현석.py
@@ -0,0 +1,12 @@
+def gcd(x, y): # 유클리드 호제법을 이용한 최대공약수 구하는 함수
+    if x % y == 0: 
+        return y
+    else: # 나머지가 존재하면 재귀적으로 호출
+        return gcd(y, x % y)
+
+a,b = map(int, input().split())
+a = max(a, b)
+b = min(a, b)
+res1 = gcd(a, b) 
+res2 = round((a * b) / res1) # a * b == 최대공약수 * 최소공배수
+print(res1, res2, sep='\n') 
