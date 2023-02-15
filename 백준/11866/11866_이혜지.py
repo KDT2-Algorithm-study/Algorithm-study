cb4c83e81aeaa12231daef969227ec8465b114db
hazziiss <lhj25846@naver.com>
2023-02-15 21:39:32 +0900

    Add 11866_이혜지

--- 백준/11866/11866_이혜지.py
+++ 백준/11866/11866_이혜지.py
@@ -4,11 +4,9 @@ n, k = map(int, input().split())
 dq = deque(range(1, n+1))
 
 print('<', end="")
-for t in range(n) :
+for t in range(n-1) :
     for _ in range(k-1) :
-        dq.append(dq.popleft())
-    if t == n-1:
-        break    
+        dq.append(dq.popleft())  
     print(f'{dq.popleft()}, ', end="")    
 
 print(f'{dq.popleft()}>', end="")    
