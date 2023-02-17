3dddbd42a81d251316ecc4d9ba680ff060cf2698
hazziiss <lhj25846@naver.com>
2023-02-17 21:39:21 +0900

    Add 1018_이혜지

---
+++ 백준/1018/1018_이혜지.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+original = []
+count = []
+
+for _ in range(N):
+    original.append(input())
+
+for a in range(N-7):
+    for b in range(M-7):
+        index1 = 0
+        index2 = 0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i+j) % 2 == 0:
+                    if original[i][j] != 'W':
+                        index1 += 1
+                    if original[i][j] != 'B':
+                        index2 += 1
+                else:
+                    if original[i][j] != 'B':
+                        index1 += 1
+                    if original[i][j] != 'W':
+                        index2 += 1
+        count.append(min(index1, index2))
+
+print(min(count))
+# 오늘 문제는 접근도 못하겠어서 답지 찾아봤습니다,,
+# 열심히 해서 실력을 향상시키겠습니다,, 
