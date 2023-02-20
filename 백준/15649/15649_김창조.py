36a8f7fee1eba698e92e025de6e6a8cd240342fd
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-02-20 12:11:29 +0900

    Fix 15649_김창조

--- 백준/15649/15649_김창조.py
+++ 백준/15649/15649_김창조.py
@@ -1,11 +1,11 @@
 # 15649. N과 M (1)
 
 import sys
-from itertools import combinations
+from itertools import permutations
 
 n, c = map(int, sys.stdin.readline().rstrip().split())
 
-comb_lst = list(combinations([num for num in range(1, n+1)], c))
+comb_lst = list(permutations([num for num in range(1, n+1)], c))
 
 for lst in comb_lst:
     print(*lst)
