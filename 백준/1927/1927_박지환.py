9daecd252470db342da5f4464edd8964c22e49eb
ParkJjHwan22 <bat522@naver.com>
2023-03-02 11:52:19 +0900

    1927_박지환.py

---
+++ 백준/1927/1927_박지환.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+command = [int(sys.stdin.readline()) for _ in range(n)]
+heap = []
+
+for i in command:
+    if i == 0:
+        if len(heap) != 0:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, i)
