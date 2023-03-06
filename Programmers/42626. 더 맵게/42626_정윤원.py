15d4a01b3fa288715f43e7476768b884b686fa12
jeongyun11 <uwn1004@gmail.com>
2023-03-06 18:59:55 +0900

    Fix 42626_정윤원.py

--- Programmers/42626. 더 맵게/42626_정윤원.py
+++ Programmers/42626. 더 맵게/42626_정윤원.py
@@ -3,14 +3,11 @@ import heapq
 def solution(scoville, K):
     cnt = 0
     heapq.heapify(scoville)
-    # 만약 스코빌이 만족되면 멈춘다
+    
     for i in range(len(scoville)-1) : 
-        if scoville[0] >= K : break
-        food1 = heapq.heappop(scoville)
-        food2 = heapq.heappop(scoville)
-        heapq.heappush(scoville, food1 + food2 * 2)
+        if scoville[0] >= K : break # 만약 스코빌이 만족되면 멈춘다
+        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
         cnt += 1
 
     if scoville[0] >= K : return(cnt)
     else : return(-1)
-    
