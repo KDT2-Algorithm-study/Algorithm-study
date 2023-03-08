1956e4aed4cb8950adb04a268e979fa1c12f3d9b
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-03-08 16:32:37 +0900

    Add 1284_김창조

---
+++ SWEA/1284/1284_김창조.py
@@ -0,0 +1,16 @@
+# 1284. 수도 요금 경쟁
+
+test_case = int(input())
+
+for T in range(1, test_case+1):
+    answer = 0
+    
+    p, q, r, s, w = map(int, input().split())
+    
+    a_charge = p * w
+    # r should be less than w
+    b_charge = q if w <= r else (q + s*(w-r))
+    
+    answer = min(a_charge, b_charge)
+    
+    print(f"#{T} {answer}")
