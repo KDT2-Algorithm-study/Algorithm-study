c2067818479e3a1daca4c0909b1faba74a522c93
pty9714 <pty9714@naver.com>
2023-04-04 23:39:16 +0900

    Add 76501_박태양

---
+++ Programmers/76501. 음양 더하기/76501_박태양.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i]==True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    
+    return answer
