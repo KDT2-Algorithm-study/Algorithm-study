cad595bfec392e14e2f896f3283c4d63914d8e2b
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-04-04 09:16:24 +0900

    Add 76501_김범서

---
+++ Programmers/76501. 음양 더하기/76501_김범서.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    answer = 0
+
+    for i in range(len(signs)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+
+    return answer
