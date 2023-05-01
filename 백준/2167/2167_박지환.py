9e96297d8d0a1b78430b227029ec936f01e2f4bf
ParkJjHwan22 <bat522@naver.com>
2023-05-01 22:46:41 +0900

    Add 2167_박지환

--- 백준/2167/2167_박지환.py
+++ 백준/2167/2167_박지환.py
@@ -1,9 +1,10 @@
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-k = int(sys.stdin.readline())
+k = int(input())
 dp = [[0] * (m + 1) for _ in range(n+1)]
 
 for r in range(1, n+1):
@@ -11,5 +12,5 @@ for r in range(1, n+1):
         dp[r][c] = arr[r-1][c-1] + dp[r][c-1] + dp[r-1][c] - dp[r-1][c-1]
 
 for _ in range(k):
-    i, j, x, y = map(int, sys.stdin.readline().split())
+    i, j, x, y = map(int, input().split())
     print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1] )
