0a270173c26be1c5157c5ebdd082cfd09e64cb72
mungjimangji <mungjmangji@hphk.com>
2023-05-08 16:09:13 +0900

    Add 1009_장민지

---
+++ 백준/1009/1009_장민지.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+for _ in range(T):
+    a, b = map(int, input().split())
+    a = a % 10
+    
+    if a == 0:
+        print(10)
+    elif a == 1 or a == 5 or a == 6:
+        print(a)
+    elif a == 4 or a == 9:
+        b = b % 2
+        if b == 1:
+            print(a)
+        else:
+            print((a * a) % 10)
+    else:
+        b = b % 4
+        if b == 0:
+            print(a**4%10)
+        else:
+            print(a**b%10)
